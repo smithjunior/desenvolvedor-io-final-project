8da7b5d78e71fa47b35bc9f57e0c94463761c486
Smith Júnior <smith.junior@icloud.com>
2020-06-21 17:11:57 -0300

    register User has been created

--- src/app/conta/cadastro/cadastro.component.ts
+++ src/app/conta/cadastro/cadastro.component.ts
@@ -1,14 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-
+import { ValidationMessages, GenericValidator, DisplayMessage } from './../../utils/generic-form-validation';
+import { ContaService } from './../services/conta.service';
+import { Usuario } from './../models/usuario';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, FormControlName } from '@angular/forms';
+import { CustomValidators } from 'ngx-custom-validators';
+import { Observable, merge, fromEvent } from 'rxjs';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
 })
-export class CadastroComponent implements OnInit {
+export class CadastroComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChildren(FormControlName, {read: ElementRef}) formInputElements: ElementRef[];
+
+  errors: any[] = []
+  cadastroForm: FormGroup;
+  usuario: Usuario;
+
+  validationMessages: ValidationMessages;
+  genericValidator: GenericValidator;
+  displayMessage: DisplayMessage = {};
+
+  constructor(private fb:FormBuilder,
+              private contaService: ContaService) {
+    this.validationMessages =  {
+      email: {
+        required: 'Informe o e-mail',
+        email: 'Email inválido'
+      },
+      password: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      confirmPassword: {
+        required: 'Informe a senha novamente',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      }
+    };
+    // Instanciando o validador senão a generic não funciona
+    this.genericValidator = new GenericValidator(this.validationMessages);
+
+  }
 
   ngOnInit(): void {
+    let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6,15])]);
+    let senhaConfirm = new FormControl('', [Validators.required, CustomValidators.rangeLength([6,15]), CustomValidators.equalTo(senha) ]);
+
+    this.cadastroForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: senha,
+      confirmPassword: senhaConfirm
+    });
+  }
+
+  ngAfterViewInit(): void {
+    let controlBlurs: Observable<any>[] = this.formInputElements
+    .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+    merge(...controlBlurs).subscribe(()=>{
+      this.displayMessage = this.genericValidator.processarMensagens(this.cadastroForm);
+//       this.mudancasNaoSalvas = true;
+    });
+  }
+
+  registrarConta() {
+    if(this.cadastroForm.dirty && this.cadastroForm.valid) {
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+      this.contaService.registrarUsuario(this.usuario);
+    }
   }
 
 }
--- src/app/conta/conta.module.ts
+++ src/app/conta/conta.module.ts
@@ -1,12 +1,14 @@
+import { ContaService } from './services/conta.service';
 import { ContaAppComponent } from './conta.app.component';
 import { RouterModule } from '@angular/router';
 import { ContaRoutingModule } from './conta.route';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {CustomFormsModule} from 'ngx-custom-validators';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ContaRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CustomFormsModule
+  ],
+  providers: [
+    ContaService
   ]
 })
 export class ContaModule { }
